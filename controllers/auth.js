5021ed49e2d8e4ece7d4157a92439b70febf63e4
Nontanun Ausungnoen <nunx111@gmail.com>
2025-04-20 19:35:03 +0700

    update auth controller and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,89 +1,126 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 //@desc     Register user
 //@route    POST /api/v1/auth/register
 //@access   Public
-exports.register= async (req,res,next)=>{
-    try{
-        const { name, email, password, telephone, role} = req.body;
-        const user = await User.create({
-            name,
-            email,
-            password,
-            telephone,
-            role
-        });
-        
-        sendTokenResponse(user,200,res);
-    } catch(err){
-        res.status(400).json({success:false});
-        console.log(err.stack);
-    }
-}
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password, telephone, role } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+      telephone,
+      role,
+    });
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
+};
+
+// controllers/users.js
+exports.addTelephone = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { telephone: req.body.telephone },
+      { new: true, runValidators: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
 
 //@desc     Login user
 //@route    POST /api/v1/auth/login
 //@access   Public
-exports.login = async (req,res,next) => {
-    const { email, password } = req.body;
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
 
-    // Validate email & password
-    if( !email || !password ){
-        return res.status(400).json({success:false, msg:'Please provide an email and password'});
-    }
+  // Validate email & password
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide an email and password" });
+  }
 
-    // Check for user
-    const user = await User.findOne({email}).select('+password');
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
-        return res.status(400).json({success:false, msg:'Invalid credentials'});
-    }
+  if (!user) {
+    return res.status(400).json({ success: false, msg: "Invalid credentials" });
+  }
 
-    // Check if password matches
-    const isMatch = await user.matchPassword(password);
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
-        return res.status(401).json({success:false, msg:'Invalid credentials'});
-    }
+  if (!isMatch) {
+    return res.status(401).json({ success: false, msg: "Invalid credentials" });
+  }
 
-    // Create token
-    //const token = user.getSignedJwtToken();
-    //res.status(200).json({success:true, token})
-    sendTokenResponse(user,200,res);
-}
+  // Create token
+  //const token = user.getSignedJwtToken();
+  //res.status(200).json({success:true, token})
+  sendTokenResponse(user, 200, res);
+};
+
+//@desc     Log user out / clear cookie
+//@route    GET /api/v1/auth/logout
+//@access   Private
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+};
 
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
+  // Create token
+  const token = user.getSignedJwtToken();
 
-    const options = {
-        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
-        httpOnly: true
-    };
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
 
-    if(process.env.NODE_ENV==='production'){
-        options.secure = true;
-    }
-    res.status(statusCode).cookie('token',token,options).json({
-        success: true,
-        // add for frontend
-        _id: user._id,
-        name: user.name,
-        email:user.email,
-        telephone: user.telephone,
-        // end for frontend
-        token
-    })
-}
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    // add for frontend
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    telephone: user.telephone,
+    // end for frontend
+    token,
+  });
+};
 
 //@desc     Get current Logged in user
 //@route    POST /api/v1/auth/me
 //@access   Private
-exports.getMe = async ( req, res, next ) => {
-    const user = await User.findById(req.user.id);
-    res.status(200).json({
-        success:true,
-        data:user
-    })
-}
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { register, login, getMe } = require("../controllers/auth");
+const { register, login, getMe, logout, addTelephone } = require("../controllers/auth");
 
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
 
+router.post('/add-telephone', protect, addTelephone);
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.get('/logout', logout);
 
 module.exports = router;
