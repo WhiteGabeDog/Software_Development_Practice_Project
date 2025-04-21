a30985ae56374d793ad34cf704d8d815c0cd39a9
Nontanun Ausungnoen <nunx111@gmail.com>
2025-04-21 19:48:23 +0700

    add telephone and authprovider to user model

--- models/User.js
+++ models/User.js
@@ -1,60 +1,77 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: [true, 'Please add a name']
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please add a valid email",
+    ],
+  },
+  authProvider: {
+    type: String,
+    enum: ["local", "google"],
+    default: "local",
+  },
+  password: {
+    type: String,
+    required: function () {
+      return this.authProvider === "local";
     },
-    email:{
-        type: String,
-        required: [true, 'Please add an email'],
-        unique: true,
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please add a valid email'
-            ]
+    minlength: 6,
+    select: false,
+  },
+  telephone: {
+    type: String,
+    required: function () {
+      return this.authProvider === "local";
     },
-    telephone: {
-        type: String,
-        required: [true, 'Please add a telephone number'],
-        match: [/^\d{10,15}$/, 'Please add a valid telephone number']
-    },
-    role: {
-        type: String,
-        enum: ['user','admin'],
-        default: 'user'
-    },
-    password: {
-        type: String,
-        required: [true, 'Please add a password'],
-        minlength: 6,
-        select: false
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-    createAt:{
-        type: Date,
-        default: Date.now
-    }
-})
-
-//Encrpyt password using bcrypt
-UserSchema.pre('save',async function(next){
-    const salt=await bcrypt.genSalt(10);
-    this.password=await bcrypt.hash(this.password,salt);
+    match: [/^\d{10,15}$/, "Please add a valid telephone number"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  refreshToken: {
+    type: String,
+    default: null,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-UserSchema.methods.getSignedJwtToken=function() {
-    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRE
-    });
-}
+//Encrpyt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  if (this.authProvider === "google" || !this.isModified("password")) {
+    return next();
+  }
 
-UserSchema.methods.matchPassword=async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password);
-}
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-module.exports = mongoose.model('User',UserSchema);
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
